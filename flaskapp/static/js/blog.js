9b2a562272d02f6b5640f7af21ec9c7ba82dacc9
yegorkowalew <kowalew.backup@gmail.com>
2018-11-07 00:23:50 +0200

    упрощенная сборка меню и слайдера

--- flaskapp/static/js/blog.js
+++ flaskapp/static/js/blog.js
@@ -24,7 +24,7 @@ function showMenu() {
             // var txt = "<div class="carousel-item"><img class="first-slide" src="static/img/i2.jpg" alt="First slide"><div class="container"><div class="carousel-caption text-left"><h1>Программирование <span class="badge badge-light">2</span></h1><p>Программирование на Python, JavaScript</p><p><a class="btn btn-lg btn-primary" href="#" role="button">Перейти</a></p></div></div></div>";
             // var txt = document.write("<h1>Привет мир!</h1>");
             data_slide++
-            $(".carousel-inner").append(`<div class="carousel-item"><img class="first-slide" src="/static/img/i2.jpg" alt="First slide"><div class="container"><div class="carousel-caption text-left"><h1>${val.title_ru} <span class="badge badge-light">${val.summ}</span></h1><p>${val.description}</p><p><a class="btn btn-lg btn-primary" href="${val.title_slug}" role="button">Перейти</a></p></div></div></div>`)
+            $(".carousel-inner").append(`<div class="carousel-item"><img class="first-slide" src="${val.img}" alt="First slide"><div class="container"><div class="carousel-caption text-left"><h1>${val.title_ru} <span class="badge badge-light">${val.summ}</span></h1><p>${val.description}</p><p><a class="btn btn-lg btn-primary" href="${val.title_slug}" role="button">Перейти</a></p></div></div></div>`)
             // .html('<div class="carousel-item"><img class="first-slide" src="/static/img/i2.jpg" alt="First slide"><div class="container"><div class="carousel-caption text-left"><h1>Программирование <span class="badge badge-light">2</span></h1><p>Программирование на Python, JavaScript</p><p><a class="btn btn-lg btn-primary" href="#" role="button">Перейти</a></p></div></div></div>');
         });
        }).done(function( json ) {
